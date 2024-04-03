44fcdb36e8d1c1c78cffd2d8340743ee240fcf2d
Abdel <abdel-madjid.santanna@polymtl.ca>
2024-04-03 16:30:13 -0400

    [dev] precomputed histograms to reduce execution time by miles

--- INF8770_H2024_TP3/moodle/src/algo3_euclide.py
+++ INF8770_H2024_TP3/moodle/src/algo3_euclide.py
@@ -29,6 +29,7 @@ class Algo3Euclide:
         self.frames_dir = FRAME_DIR
         self.hist_size = hist_size
         self.image_dir = IMAGE_DIR
+        self.frame_histograms:dict = {}
         if not frame_sampled:
             sample_frames()
     
@@ -75,29 +76,35 @@ class Algo3Euclide:
         return np.linalg.norm(hist1 - hist2)
     
     #TODO ouvrir le dir des frames et déterminer la frame qui a la plus petite distance euclidienne par rapport à l'image
-    def get_closest_frame(self,image_path):# builds the set of euclidian distances
-        isfound = False
-        image_hist = self.create_histogram(image_path)
-        closest_frame = FrameData("", "", TRESHOLD)
+    def precompute_histograms(self):
         for video_dir in os.listdir(self.frames_dir):
             full_video_dir = os.path.join(self.frames_dir, video_dir)
             for frame in os.listdir(full_video_dir):
                 frame_path = os.path.join(full_video_dir, frame)
                 frame_hist = self.create_histogram(frame_path)
-                if(len(frame_hist) != len(image_hist)): continue
-                distance = self.euclidean_distance(image_hist, frame_hist)
-                if distance < closest_frame.distance:
-                    closest_frame.distance = distance
-                    closest_frame.video_name = video_dir
-                    closest_frame.frame_index = frame
-                    isfound = True
-                    #TODO determine a treshold to not consider the frame if the distance is too high
+                # Store the histogram using the frame path as the key
+                self.frame_histograms[frame_path] = frame_hist
+
+    def get_closest_frame(self, image_path):
+        isfound = False
+        image_hist = self.create_histogram(image_path)
+        closest_frame = FrameData("", "", TRESHOLD)
+        for frame_path, frame_hist in self.frame_histograms.items():
+            if(len(frame_hist) != len(image_hist)): continue
+            distance = self.euclidean_distance(image_hist, frame_hist)
+            if distance < closest_frame.distance:
+                closest_frame.distance = distance
+                closest_frame.video_name = os.path.dirname(frame_path)
+                closest_frame.frame_index = os.path.basename(frame_path)
+                isfound = True
             if(isfound):
                 break
         return closest_frame
     
     def compare_images(self):
+        self.precompute_histograms()  # Precompute the histograms before comparing images
         for image_name in os.listdir(self.image_dir):
+            start_time = time.time()
             image_path = os.path.join(self.image_dir, image_name)
             closest_frame = self.get_closest_frame(image_path)
             print(f"Closest frame for {image_name} is {closest_frame.video_name}/{closest_frame.frame_index} with a distance of {closest_frame.distance}")
@@ -107,6 +114,9 @@ class Algo3Euclide:
                     writer.writerow([image_name, closest_frame.video_name, closest_frame.frame_index])
                 else:
                     writer.writerow([image_name, 'out', ''])
+            end_time = time.time()
+            execution_time = end_time - start_time
+            print(f"Execution time for image {image_name} is : {execution_time}")
 
     
 def main():
