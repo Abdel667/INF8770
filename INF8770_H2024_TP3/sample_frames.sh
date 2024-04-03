cbd9017f334f6dcd88adfd279fb501ff623d2879
Abdel <abdel-madjid.santanna@polymtl.ca>
2024-04-03 09:06:38 -0400

    [dev] generalized sample_frames

--- INF8770_H2024_TP3/sample_frames.sh
+++ INF8770_H2024_TP3/sample_frames.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-& C:/Users/Abdel/AppData/Local/Microsoft/WindowsApps/python3.12.exe "c:/Users/Abdel/Documents/Hiver 2024/INF8770/Labos/INF8770/INF8770_H2024_TP3/moodle/src/frame-colector.py"
+python "moodle/src/frame-colector.py"
