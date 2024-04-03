d01a99a518d6acc1bfc1df9bdd218daa7f57bf8c
Abdel <abdel-madjid.santanna@polymtl.ca>
2024-04-03 14:49:43 -0400

    [dev] euclide algo functional but too slow

--- INF8770_H2024_TP3/moodle/src/algo3_euclide.py
+++ INF8770_H2024_TP3/moodle/src/algo3_euclide.py
@@ -1,15 +1,23 @@
 # IMPORTS
+import csv
 from enum import Enum
 import os
+import time
 import cv2
 import numpy as np
-from scipy.stats import chisquare
+from frame_colector import sample_frames
+
+IMAGE_DIR = f"moodle/data/jpeg"
+VIDEO_DIR = f"moodle/data/mp4"
+TEST_CSV_PATH = f"moodle/data/test.csv"
+FRAME_DIR = f"moodle/video_frames"
+TRESHOLD = 100000
 
 #Définition de la classe générale et de ses paramètres (notament is_inside)
 class FrameData:
     def __init__(self, video_name, frame_name, distance):
         self.video_name:str = video_name
-        self.frame_name:str = frame_name
+        self.frame_index:str = frame_name
         self.distance:float = distance
 
 class CompareMethod(Enum):
@@ -17,13 +25,16 @@ class CompareMethod(Enum):
     CHI2 = "chi2"
 
 class Algo3Euclide:
-    def __init__(self, hist_size:int, frames_dir:str):
-        self.frames_dir = frames_dir
+    def __init__(self, hist_size:int=4, frame_sampled:bool = True):
+        self.frames_dir = FRAME_DIR
         self.hist_size = hist_size
-
+        self.image_dir = IMAGE_DIR
+        if not frame_sampled:
+            sample_frames()
+    
     # Fonction de Construction des histogrammes de couleurs 1D pour chaque trames (RGB ou YUV)
     #TODO sauvegarde des descripteurs des histogrammes de couleurs 1D afin de les réutiliser pour l'autre hypothèse
-    def create_histogram(self, image_path, hist_size=4):
+    def create_histogram(self, image_path):
         # Load the image
         image = cv2.imread(image_path)
 
@@ -31,6 +42,9 @@ class Algo3Euclide:
         if image is None:
             print("Could not open or find the image")
             exit(0)
+
+        if len(image.shape) == 2: # Grayscale image
+            image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
 
         # Split the image into its color channels
         channels = cv2.split(image)
@@ -42,49 +56,63 @@ class Algo3Euclide:
 
         # Calculate histograms for each channel with specified histSize
         for channel in channels:
-            hist = cv2.calcHist([channel], [0], None, [hist_size], [0, 256])
-            if channel == channels[0]: # Red channel
+            hist = cv2.calcHist([channel], [0], None, [self.hist_size], [0, 256])
+            if np.array_equal(channel, channels[0]): # Red channel
                 hist_r = hist.flatten()
-            elif channel == channels[1]: # Green channel
+            elif np.array_equal(channel, channels[1]): # Green channel
                 hist_g = hist.flatten()
             else: # Blue channel
                 hist_b = hist.flatten()
 
         # Print the histograms
-        print("Red Channel Histogram:", hist_r)
-        print("Green Channel Histogram:", hist_g)
-        print("Blue Channel Histogram:", hist_b)
+        # print("Red Channel Histogram:", hist_r)
+        # print("Green Channel Histogram:", hist_g)
+        # print("Blue Channel Histogram:", hist_b)
         return np.concatenate((hist_r, hist_g, hist_b))
     
     # Fonction poour calculer la distance euclidienne entre deux histogrammes pour mesurer leur affinité
-    def euclidean_distance(hist1, hist2):
+    def euclidean_distance(self,hist1, hist2):
         return np.linalg.norm(hist1 - hist2)
     
-    #TODO ouvrir le dir des frames et conserver pour chaque dir le nom de la vidéo et le nom de la frame 
-    #                 qui a la plus petite distance euclidienne par rapport à l'image
-    def build_set(self, video_parent_dir,image_hist):# builds the set of euclidian distances
-        closest_frame = FrameData("", "", float('inf'))
-        for video_dir in os.listdir(video_parent_dir):
-            for frame in os.listdir(video_dir):
-                frame_path = os.path.join(video_dir, frame)
+    #TODO ouvrir le dir des frames et déterminer la frame qui a la plus petite distance euclidienne par rapport à l'image
+    def get_closest_frame(self,image_path):# builds the set of euclidian distances
+        image_hist = self.create_histogram(image_path)
+        closest_frame = FrameData("", "", TRESHOLD)
+        for video_dir in os.listdir(self.frames_dir):
+            full_video_dir = os.path.join(self.frames_dir, video_dir)
+            for frame in os.listdir(full_video_dir):
+                frame_path = os.path.join(full_video_dir, frame)
                 frame_hist = self.create_histogram(frame_path)
+                if(len(frame_hist) != len(image_hist)): continue
                 distance = self.euclidean_distance(image_hist, frame_hist)
-                if distance < closest_frame['distance']:
-                    closest_frame['distance'] = distance
-                    closest_frame['video'] = video_dir
-                    closest_frame['frame'] = frame
-            
-        
-    #TODO déterminer la vidéo à laquelle apparrtient l'image et le temps le plus     
-    # proche auquel elle a été prise
-    def compare_histograms(self, hist1, hist2):
-        return self.euclidean_distance(hist1, hist2)
+                if distance < closest_frame.distance:
+                    closest_frame.distance = distance
+                    closest_frame.video_name = video_dir
+                    closest_frame.frame_index = frame
+                    #TODO determine a treshold to not consider the frame if the distance is too high
+        return closest_frame
     
+    def compare_images(self):
+        for image_name in os.listdir(self.image_dir):
+            image_path = os.path.join(self.image_dir, image_name)
+            closest_frame = self.get_closest_frame(image_path)
+            print(f"Closest frame for {image_name} is {closest_frame.video_name}/{closest_frame.frame_index} with a distance of {closest_frame.distance}")
+            with open(TEST_CSV_PATH, 'a', newline='') as file:
+                writer = csv.writer(file)
+                if closest_frame.distance < TRESHOLD:
+                    writer.writerow([image_name, closest_frame.video_name, closest_frame.frame_index])
+                else:
+                    writer.writerow([image_name, 'out', ''])
 
-
-
-# Fonction poour calculer le Chi 2 entre deux histogrammes pour mesurer leur affinité
-
-
-#Fonction de comparaison des image  vidéo
     
+def main():
+    start_time = time.time()
+    algo = Algo3Euclide()
+    algo.compare_images()
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"Execution time of algo3 with euclide is : {execution_time}")
+
+
+if __name__ == "__main__":
+    main()
